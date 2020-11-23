cb7432e3e8d4dcf93a5303c66e8f63b4ccf2cb53
david11yf29 <david11yf29@gmail.com>
2020-11-23 22:17:10 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import Enzyme, { EnzymeAdapter } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
