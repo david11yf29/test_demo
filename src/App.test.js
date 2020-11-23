743d4b3d6c0b7797d9b3847cf2c5c4bf734e1b5f
david11yf29 <david11yf29@gmail.com>
2020-11-23 22:26:07 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import Enzyme, { EnzymeAdapter } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+  expect(wrapper).toBeTruthy();
 });
