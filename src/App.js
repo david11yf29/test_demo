5dec753cb172833af0ec832d3cf2bfc354421c8d
david11yf29 <david11yf29@gmail.com>
2020-11-30 15:57:48 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = React.useState(0);
-
+function App() { 
   return (
-    <div data-test="component-app" className="App">
-      <h1 data-test="counter-display">
-        The counter is currently&nbsp; 
-        <span data-test="count">{count}</span>
-      </h1>
-      <button
-        data-test="increment-button"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment counter
-      </button>
+    <div data-test="component-app">
+
     </div>
-  );
+  )
 }
 
 export default App;
