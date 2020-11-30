950dc73294f649641e926d68dc1295ad62cbd882
david11yf29 <david11yf29@gmail.com>
2020-11-30 16:40:40 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -5,18 +5,24 @@ import App from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup = () => shallow(<App />);
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
 
 test('renders button', () => {
-
+  const wrapper = setup();
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
 });
 
 test('renders counter display',() => {
-
+  const wrapper = setup();
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
 });
 
 test('counter starts at 0',() => {
