33027a344ba1690cc08324d1098d688393ae2b30
david11yf29 <david11yf29@gmail.com>
2020-12-03 23:26:13 +0800

    updated

--- src/input.test.js
+++ src/input.test.js
@@ -4,6 +4,30 @@ import { shallow } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import Input from './input';
 
-test('', () => {
-  
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {
+
+    });
+    test('renders input box', () => {
+
+    });
+    test('renders submit button', () => {
+
+    });
+  });
+});
+
+describe('update state', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {
+
+    });
+    test('does not render input box', () => {
+
+    });
+    test('does not render submit button', () => {
+
+    });
+  });
 });
