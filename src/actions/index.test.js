000ffbec9e73687723d011ce8a64feb67ed41676
david11yf29 <david11yf29@gmail.com>
2020-12-04 19:03:29 +0800

    updated

--- src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './index';
-
-describe('correctGuess', () => {
-  test('returns an actions with type "CORRECT_GUESS"', () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  })
-});
---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions/index';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updated state correctly for unsuccessful guess', () => {
+
+    });
+    test('updated state correctly for successful guess', () => {
+      
+    })
+  })
+  describe('some guessed words', () => {
+    test('updated state correctly for unsuccessful guess', () => {
+
+    });
+    test('updated state correctly for successful guess', () => {
+      
+    })
+  })
+})
