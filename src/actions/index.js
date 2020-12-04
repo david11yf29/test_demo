1f055d05d210f3f37dcf2cad234f32a0a2f38b11
david11yf29 <david11yf29@gmail.com>
2020-12-04 20:38:53 +0800

    updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../helpers/index';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
-  GUESS_WORD: 'GUESS_WORD'
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 };
 
 export const guessWord = (guessedWord) => {
@@ -21,4 +23,10 @@ export const guessWord = (guessedWord) => {
       })
     }
   };
+}
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    axios.get('http://localhost:3030');
+  }
 }
